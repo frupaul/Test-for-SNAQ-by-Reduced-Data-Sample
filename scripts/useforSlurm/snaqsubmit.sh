bc74153e35fb78f5b251912e4449b965211564e4
Claudia Solis-Lemus <claudiar.solislemus@gmail.com>
2016-11-03 13:50:54 -0500

    added quotes

--- scripts/useforSlurm/snaqsubmit.sh
+++ scripts/useforSlurm/snaqsubmit.sh
@@ -3,5 +3,5 @@
 #SBATCH --mail-user=nji3@wisc.edu
 #SBATCH --array=1-50
 alias julia="/workspace/software/julia-0.5.0/julia"
-export JULIA_PKGDIR=/workspace/nanji/.julia
+export JULIA_PKGDIR="/workspace/nanji/.julia"
 julia oneSnaqOneRun.jl 0 $SLURM_ARRAY_TASK_ID 
