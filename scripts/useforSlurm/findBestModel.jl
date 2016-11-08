0762d38fb76cc3fa1401967718c3b97223bc6ccf
frupaul <nji3@wisc.edu>
2016-11-08 12:59:38 -0600

    Modify findBestModel.jl

--- scripts/useforSlurm/findBestModel.jl
+++ scripts/useforSlurm/findBestModel.jl
@@ -4,14 +4,16 @@ function eachRunLoglik(x)
     return b
 end
 
-file = open("snaq_h0_id_1_snaq.out");
+h = ARGS[1];
+
+file = open(string("snaq_h",h,"_id_1_snaq.out"));
 lines = readlines(file);
 a = eachRunLoglik(lines[1]);
 file = close(file);
 b = 1;
 
 for i = 1:100
-    Filename =  string("snaq_h0_id_",i,"_snaq.out");
+    Filename =  string("snaq_h",h,"_id_",i,"_snaq.out");
     file = open(Filename);
     lines = readlines(file);
     logLike = eachRunLoglik(lines[1]);
@@ -22,5 +24,9 @@ for i = 1:100
     file = close(file);
 end
 
-println(a);
-println(b);
+fileId = string("Best Loglike ", a," Best Model ID ", b);
+
+open(string("h",h,"bestModelID.txt"), "w") do f
+    write("bestModelID.txt",fileId)
+end
+
