b5e9f9b56fde5679ac82f86c0fd19da8bce0691a
frupaul <nji3@wisc.edu>
2016-11-11 10:43:08 -0600

    modified snaqsubmit.sh

--- scripts/useforSlurm/snaqsubmit.sh
+++ scripts/useforSlurm/snaqsubmit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nji3@wisc.edu
-#SBATCH --array=1-50
+#SBATCH --array=1-100
+export PERL5LIB="/s/slurm/lib/perl/5.18.2"
 export JULIA_PKGDIR="/workspace/nanji/.julia"
-/workspace/software/julia-0.5.0/julia /workspace/nanji/oneSnaqOneRun.jl 0 $SLURM_ARRAY_TASK_ID 
+/workspace/software/julia-0.5.0/julia /workspace/nanji/oneSnaqOneRun.jl 1 $SLURM_ARRAY_TASK_ID 
