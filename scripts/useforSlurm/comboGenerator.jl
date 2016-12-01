6dc14a31602abf8243e8ee36c437e29c5ed1d33a
frupaul <nji3@wisc.edu>
2016-12-01 11:07:27 -0600

    fix mistakes in combogenerator.jl

--- scripts/useforSlurm/comboGenerator.jl
+++ scripts/useforSlurm/comboGenerator.jl
@@ -1,9 +1,19 @@
+# The function was imported into oneSnaqH.jl
+# to generate 240 combinations of parameters in total.
+# It will return an array containing nubmers respect
+# to the specific parameters initialized in the
+# arraies in oneSnaqH.jl
+
 function comb(input)
     a = mod(input,5);
-    b = mod(input-a,4);
-    c = mod(input-a-b,3);
-    d = mod(input-a-b-c,2);
-    e = mod(input-a-b-c-d,2);
+    ia = (input-a)/5;
+    b = mod(ia,4);
+    ib = (ia-b)/4;
+    c = mod(ib,3);
+    ic = (ib-c)/3;
+    d = mod(ic,2);
+    id = (ic-d)/2;
+    e = mod(id,2);
     comb = [e+1,d+1,a+1,c+1,b+1];
     return comb
 end
