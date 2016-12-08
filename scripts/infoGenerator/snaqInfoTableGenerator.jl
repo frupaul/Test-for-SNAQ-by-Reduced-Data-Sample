2f65bf6f3ab12018b31ce77b5c329658167d9da6
frupaul <nji3@wisc.edu>
2016-12-08 12:45:48 -0600

    Modified snaqInfoTableGenerator.jl

--- scripts/infoGenerator/snaqInfoTableGenerator.jl
+++ scripts/infoGenerator/snaqInfoTableGenerator.jl
@@ -1,6 +1,7 @@
-Pkg.add(PhyloNetworks);
-Pkg.add(DataFrame);
-Pkg.update();
+#Pkg.add("PhyloNetworks");
+#Pkg.add("DataFrames");
+#Pkg.add("Dates")
+#Pkg.update();
 
 if length(ARGS) != 1
   error("need 1 parameter: the address of the directory")
@@ -17,12 +18,14 @@ fileNames = filter(x->contains(x,".log"),readdir(pwd())); # Read only the names 
 # The function is used to read the starting time of two runs and return the
 # difference which is the consuming time for one run.
 
+using Dates;
+
 function eachRunTimeSummary(x,y)
     dat1 = DateTime(x, "e d u y H:M:S") - (
                contains(x, "AM") ? Hour(12) : Hour(0));
     dat2 = DateTime(y, "e d u y H:M:S") - (
                contains(y, "AM") ? Hour(12) : Hour(0));
-    Diff = Int(dat2 - dat1)/(60*1000);
+    Diff = Float64(dat2 - dat1)/1000;
     return Diff
 end
 
@@ -32,8 +35,9 @@ parameterInfo = Array{String}(8); # The array to save eight parameters: hmax, ft
 parameterInfoList = Array{String}[]; # The arrayList to save the parameter array for each file
 runSeed = Array{String}[]; # The arrayList used to store seed for each run
 runLoglik = Array{String}[]; # The arrayList used to store loglikValue for each run
-runTimeList = Array{String}[]; # The arrayList used to store the time for each run
+runTimeList = Array{Float64}[]; # The arrayList used to store the time for each run
 runTopology = Array{String}[]; # The arrayList used to store the topology for each run
+run = 0; # Count the total run number of all files
 
 # Use a for loop to find information from each log file.
 
@@ -51,6 +55,8 @@ for i in fileNames[1:end]
     parameterInfo[8] = readstring(pipeline(`grep -Eo "BEGIN: .*" $i`, `sed -E 's/BEGIN: (.*) run.*/\1/'`)); # Find number of runs
 
     push!(parameterInfoList,parameterInfo); # Save the parameter array of one file into the arrayList
+
+    run = run + parse(Int,parameterInfo[8]);
 
     # Look for all the information we need for each run in one log file.
 
@@ -70,15 +76,78 @@ for i in fileNames[1:end]
     end
     push!(runTopology,topology); # Save the topology for future comparison
 
+    # Save the info of time in each run.
+
     runTime = Array{String}(parse(Int, parameterInfo[8])+1);
     runTimeLine = split(readstring(`grep -A 1 "seed:.*" $i`), "\n")
-    for k in 1:length(runTime)-1
+    for k in 1:parse(Int, parameterInfo[8])
         runTime[k] = runTimeLine[3*k-1];
     end
     endTimeLine = split(readstring(`grep -B 1 "MaxNet is.*" $i`),"\n");
     runTime[end] = endTimeLine[1];
-    push!(runTimeList,runTime);
+
+    # Count the time used for each run.
+
+    runTimeNum = Array{Float64}(parse(Int, parameterInfo[8]));
+    for j in length(runTimeNum)
+        runTimeNum[j] = eachRunTimeSummary(runTime[j],runTime[j+1]);
+    end
+
+    push!(runTimeList,runTimeNum); # Save the time info of runs in each file
 
 end
+
+# Use dataframe to help generate the table including the information of each run.
+
+using DataFrames; # Contain the function DataFrame
+
+# Initialize the arraies saving the information used as columns in DataFrame.
+
+Hmax = Array{Int}(run);
+Nfail = Array{Int}(run);
+ftolRel = Array{Float64}(run);
+ftolAbs = Array{Float64}(run);
+xtolAbs = Array{Float64}(run);
+xtolRel = Array{Float64}(run);
+liktolAbs = Array{Float64}(run);
+
+# Initialize the array so that it could be easy to combine in for loop.
+
+seed = runSeed[1];
+sameToBestTopology = Array{Int}(run);
+loglik = runLoglik[1];
+runTime = runTimeList[1];
+
+# Repetitively set the parameters for each run.
+
+for i in 1:length(parameterInfoList)
+    for j in 1:parse(Int,parameterInfoList[i][8])
+
+        Hmax[(i-1)*j+j] = parse(Float64, parameterInfoList[i][1]);
+        Nfail[(i-1)*j+j] = parse(Float64, parameterInfoList[i][6]);
+        ftolRel[(i-1)*j+j] = parse(Float64, parameterInfoList[i][2]);
+        ftolAbs[(i-1)*j+j] = parse(Float64, parameterInfoList[i][3]);
+        xtolAbs[(i-1)*j+j] = parse(Float64, parameterInfoList[i][4]);
+        xtolRel[(i-1)*j+j] = parse(Float64, parameterInfoList[i][5]);
+        liktolAbs[(i-1)*j+j] = parse(Float64, parameterInfoList[i][7]);
+
+    end
+end
+
+# Combine all the values in the order of the runs.
+
+for i in 2:length(fileNames)
+
+    seed = [seed;runSeed[i]];
+    loglik = [loglik;runLoglik[i]];
+    runTime = [runTime;runTimeList[i]];
+
+end
+
+# Generate the DataFrame and write it into a csv file.
+
+df = DataFrame(hmax=Hmax,nfail=Nfail,ftolRel=ftolRel,ftolAbs=ftolAbs,xtolAbs=xtolAbs,xtolRel=xtolRel,liktolAbs=liktolAbs,seed=seed,loglik=loglik,runTime=runTime);
+writetable("output.csv",df)
+
 
 
