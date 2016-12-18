444edd221d759739bb48505b14be8d4324b914ca
frupaul <nji3@wisc.edu>
2016-12-17 21:42:52 -0600

    Modfied oneSnaqH_submit file

--- scripts/useforSlurm/oneSnaqH_snaqsubmit.sh
+++ scripts/useforSlurm/oneSnaqH_snaqsubmit.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+#SBATCH -o 240Outputfile.out
+#SBATCH -D /workspace/nanji/240combo
+#SBATCH -J n15perfect_240combinations
+#SBATCH -p darwin
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nji3@wisc.edu
 #SBATCH --array=1-240
 export PERL5LIB="/s/slurm/lib/perl/5.18.2"
 export JULIA_PKGDIR="/workspace/nanji/.julia"
-/workspace/software/julia-0.5.0/julia /workspace/nanji/oneSnaqH.jl 3 30 $SLURM_ARRAY_TASK_ID 
+/workspace/software/julia-0.5.0/julia /workspace/nanji/240combo/oneSnaqH.jl 3 30 $SLURM_ARRAY_TASK_ID 
